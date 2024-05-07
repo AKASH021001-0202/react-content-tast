dec52cd0409b68a3e822bf94bc7ba4adcd601680
AKASH021001-0202 <nakashn007@gmail.com>
2024-05-07 13:16:28 +0530

    third

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
   const handleQuantityChange = (itemId, quantity) => {
     setQuantities({
       ...quantities,
-      [itemId]: quantity
+      [itemId]: quantity,
     });
   };
 
@@ -19,11 +19,23 @@ const Cart = () => {
     return quantity * item.price;
   };
 
-  const subtotal = cartItems.reduce((acc, item) => acc + calculateSubtotal(item), 0);
+  const subtotal = cartItems.reduce(
+    (acc, item) => acc + calculateSubtotal(item),
+    0
+  );
 
   return (
     <div>
-      <h2>Cart</h2>
+      <header className="bg-dark text-white">
+        <div className="container">
+          <div className="header-content ">
+            <h1>Cart</h1>
+            <Link to={"/"}>
+              <button className="btn-cart"> Back</button>
+            </Link>
+          </div>
+        </div>
+      </header>
       <div className="container">
         <div className="row">
           <div className="col-md-10 mx-auto  p-5 rounded">
@@ -34,21 +46,28 @@ const Cart = () => {
                   key={index}
                 >
                   <div className="col-md-3 d-flex justify-content-center">
-                    <img className="img-thumbnail" src={item.thumbnail} alt="" />
+                    <img
+                      className="img-thumbnail"
+                      src={item.thumbnail}
+                      alt=""
+                    />
                   </div>
                   <div className="col-md-6">
                     <h3>{item.title}</h3>
                     <p>{item.description}</p>
                     <StarRating rating={item.rating} />
-                    
                   </div>
                   <div className="col-md-3">
                     <select
                       value={quantities[item.id] || 1}
-                      onChange={(e) => handleQuantityChange(item.id, parseInt(e.target.value))}
+                      onChange={(e) =>
+                        handleQuantityChange(item.id, parseInt(e.target.value))
+                      }
                     >
                       {[...Array(10).keys()].map((num) => (
-                        <option key={num + 1} value={num + 1}>{num + 1}</option>
+                        <option key={num + 1} value={num + 1}>
+                          {num + 1}
+                        </option>
                       ))}
                     </select>
                     <h6>{item.price}</h6>
@@ -62,7 +81,22 @@ const Cart = () => {
                 </div>
               ))
             ) : (
-              <div className="col-md-12">No items in the cart Return to <Link to={"/"}>Product Page</Link></div>
+              <div className="col-md-12">
+                <div className="row">
+                  <div className="col-lg-12 dflex-centered">
+                    <img
+                      className="img-product"
+                      src="https://static.vecteezy.com/system/resources/previews/005/073/073/non_2x/no-item-in-the-shopping-cart-add-product-click-to-shop-now-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg"
+                      alt="Empty Cart"
+                    />
+                  </div>
+                  <div className="col-lg-12 dflex-centered">
+                    <Link to={"/"}>
+                      <button className="btn btn-primary">Product Page</button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             )}
             <div className="row">
               <div className="col-md-12 d-flex justify-content-between">
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { CartContext } from "../contexts/CartContext";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import StarRating from "./StarRating";
 
 
@@ -20,26 +20,26 @@ const Products = (products) => {
         <div className="container">
           <div className="header-content ">
             <h1>Product</h1>
-            <button className="btn-cart"> Cart</button>
+          <Link to={"Cart"}><button className="btn-cart"> Cart</button></Link>
           </div>
         </div>
       </header>
       <div className="container">
         <div className="row">
-          <div className="col-md-10 mx-auto  p-5 rounded">
             {items.map((item, index) => (
+          <div className="col-md-6 mx-auto  p-5 rounded">
               <div
-                className="product-box row my-3  p-3 bg-light rounded"
+                className="product-box row   p-3  rounded"
                 key={index}
               >
-                <div className="col-md-3 d-flex justify-content-center">
+                <div className="col-md-4 d-flex justify-content-center">
                   <img
                     className="img-thumbnail"
                     src={item.thumbnail}
                     alt=""
                   />
                 </div>
-                <div className="col-md-9">
+                <div className="col-md-8">
                   <h3>{item.title}</h3>
                   <p>{item.description}</p>
                   <StarRating
@@ -52,9 +52,10 @@ const Products = (products) => {
                     Buy Now
                   </button>
                 </div>
+           
               </div>
-            ))}
           </div>
+           ))}
         </div>
       </div>
     </div>
